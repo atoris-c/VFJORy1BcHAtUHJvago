5a00e814ec2fc3679454bb38827ee813e164aa48
Atoris <pattarapol374.mc@gmail.com>
2025-04-27 11:28:27 +0700

    updated minimum SDK version from 21 to 29 to fully support temperature indicator

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.sknproj.qrng"
-        minSdk = 21
+        minSdk = 29
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
